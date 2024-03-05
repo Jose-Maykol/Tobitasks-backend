fbd5453fa4405421f6ffefbd953f4c603112b669
José Maykol <jpaniura@unsa.edu.pe>
2024-03-04 23:56:57 -0500

    feat: add CORS configuration and websocket adapter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    credentials: true,
+  });
+  app.useWebSocketAdapter(new IoAdapter(app));
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }

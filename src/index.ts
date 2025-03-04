ced08749eacac0749611d84ae3a9e0ab24102f83
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-03-04 00:52:55 -0500

    feat: enhance server configuration with middleware and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-const app = express();
-const port = 3000;
+dotenv.config()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with TypeScript!');
-});
+const app = express()
+const port = process.env.PORT || 3000
+
+// Middlewares
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', (req: Request, res: Response, next: NextFunction) => {
+	console.log(`Request URL: ${req.originalUrl}`)
+	next()
+})
+
+// Rutas
+app.get('/api', (req: Request, res: Response) => {
+	res.send('Tobitasks API')
+})
+
+// Manejo de errores
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+	res.status(500).send('Something went wrong!')
+})
+
+// Inicializa el servidor
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+	console.log(`Server is running on http://localhost:${port}`)
+})
